91a7aedfd211246c6269ef22176eb62ed8b9fda8
Ronald Brachetti <ronald_brachetti@mckinsey.com>
2018-05-22 17:05:46 +0400

    test with junit output

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
 library(testthat)
 library(rcicddemo)
 
-test_check("rcicddemo")
+# test_check("rcicddemo")
+test_check("rcicddemo",
+           reporter = JunitReporter$new(file = "junit_result.xml"))
